be83daed19bd14c8ff5607951d4682962a643da1
Michael Poon <107957557+mipoon@users.noreply.github.com>
2025-03-07 04:39:01 +0000

    Test button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+function MyButton() {
+  return <button>
+    I'm a button
+  </button>
+}
+
 function App() {
     return (
       <div className="App">
         <h1>Hello, World!</h1>
         <p>This is a sample for now.</p>
+        <MyButton />
       </div>
     );
   }
